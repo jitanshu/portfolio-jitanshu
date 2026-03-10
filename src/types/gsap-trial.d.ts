5d0431240e61d8b85505f17e8070dc15620056f9
jitanshu <jitanshuk6@gmail.com>
2026-03-10 22:00:55 +0530

    build fixes

---
+++ src/types/gsap-trial.d.ts
@@ -0,0 +1,19 @@
+declare module "gsap-trial/SplitText" {
+  export interface SplitTextConfig {
+    type?: string;
+    linesClass?: string;
+    wordsClass?: string;
+    charsClass?: string;
+  }
+
+  export class SplitText {
+    constructor(targets: string | string[] | Element | Element[], vars?: SplitTextConfig);
+
+    chars: HTMLElement[];
+    words: HTMLElement[];
+    lines: HTMLElement[];
+
+    split(vars?: SplitTextConfig): void;
+    revert(): void;
+  }
+}
